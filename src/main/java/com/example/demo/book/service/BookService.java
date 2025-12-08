33694b2742d74ebdac57bc4baf9a8cfb95c9144e
hcoh <hcoh990310@gmail.com>
2025-12-08 17:29:22 +0900

    도서 LMPL코드

--- src/main/java/com/example/demo/book/service/BookService.java
+++ src/main/java/com/example/demo/book/service/BookService.java
@@ -3,6 +3,7 @@ package com.example.demo.book.service;
 import com.example.demo.book.dto.BookListResponse;
 import com.example.demo.book.dto.BookRequest;
 import com.example.demo.book.dto.BookResponse;
+import com.example.demo.book.entity.Book;
 
 public interface BookService {
 
